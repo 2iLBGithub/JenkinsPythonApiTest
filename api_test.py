1167f3db8b0161d2575c136baa2435c7026b671a
Lewis Brennan <lewis.brennan@2itesting.com>
2023-11-24 13:53:30 +0000

    stuff

--- api_test.py
+++ api_test.py
@@ -2,7 +2,7 @@ import requests
 
 def test_api():
     response = requests.get('https://api.publicapis.org/entries')
-    assert response.status_code == 200, f"Oh no! Not again! Your API call failed with status: {response.status_code}. Do not give up!"
+    assert response.status_code == 200, f"Oh no! Not again! Your API call failed with status: {response.status_code}. Do not give up! Maybe."
 
 if __name__ == "__main__":
     test_api()
